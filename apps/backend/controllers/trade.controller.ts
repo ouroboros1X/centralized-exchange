b0c73e291f162f3318abe46964d5778456d6890c
ouroboros1X <devs.ouroboros@gmail.com>
2026-09-25 22:03:06 +0530

    market route and controller

--- apps/backend/controllers/trade.controller.ts
+++ apps/backend/controllers/trade.controller.ts
@@ -3,12 +3,27 @@ import type{Request , Response} from "express";
 
 
 const userTrade = (req:Request, res:Response) =>{
-
-  res.status(200).json({msg:"this is users trade Details"});
+  try {
+    res.status(200).json({
+      msg:"this is users trade Details"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 };
 
 const userTradeById = (req:Request, res:Response) => {
-  res.status(200).json({msg:"this is the users trade details ny ID"});
+  try {
+    res.status(200).json({
+      msg:"this is the users trade details ny ID"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 };
 
 
--- apps/backend/src/routes/market.routes.ts
+++ apps/backend/src/routes/market.routes.ts
@@ -1,2 +1,15 @@
-//GET /
-// GET / :SYMBOL
+import express  from "express";
+const router  =express.Router();
+
+import {
+  getMarketDetails,
+  getMarketDetailsBySymbol
+  
+} from "../../controllers/market.controller"
+
+
+router.route("/marketDetails").post(getMarketDetails);
+
+router.route("/marketDetails/:symbol").post(getMarketDetailsBySymbol);
+
+export default router;
