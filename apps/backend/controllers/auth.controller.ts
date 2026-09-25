488c748a1e06b503c5b38c9ef06c4f5387a3ab67
ouroboros1X <devs.ouroboros@gmail.com>
2026-09-25 22:07:40 +0530

    route and controller structure updated

--- apps/backend/controllers/auth.controller.ts
+++ apps/backend/controllers/auth.controller.ts
@@ -1,18 +1,39 @@
 import type {Request , Response} from "express";
 
 const registerUser = (req:Request, res:Response) =>{
-
-  res.status(200).json ({msg:"user registerd succesfully"})
+  try {
+    res.status(200).json({
+      msg:"user registerd succesfully"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 };
 
 const loginUser = (req:Request, res:Response) =>{
-
-  res.status(200).json ({msg:"user login succesfully"})
+  try {
+    res.status(200).json({
+      msg:"user login succesfully"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 };
 
 const logoutUser = (req:Request, res:Response) =>{
-
-  res.status(200).json ({msg:"user logout succesfull"})
+  try {
+    res.status(200).json({
+      msg:"user logout succesfull"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 };
 
 
--- apps/backend/controllers/order.controller.ts
+++ apps/backend/controllers/order.controller.ts
@@ -2,26 +2,55 @@ import type { Request, Response } from "express";
 
 const orderDetails  = (req: Request, res:Response) => {
 
+try {
+    res.status(200).json({
+      msg:" order details for the user  "
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }
 
-  res.status(200).json ({msg:"testing ok"});
 };
 
 const orderUser  = (req: Request, res:Response) => {
-  
+try {
+    res.status(200).json({
+      msg:"user oder succesfully"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }  
 
-  res.status(200).json ({msg:"testing ok"});
 };
 
 const orderByIdUser  = (req: Request, res:Response) => {
-  
+try {
+    res.status(200).json({
+      msg:"oder by id for user "
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }  
 
-  res.status(200).json ({msg:"testing ok"});
 };
 
 const deleteOrder  = (req: Request, res:Response) => {
-  
+try {
+    res.status(200).json({
+      msg:"order deleted ssuccesfully"
+    });
+  } catch (error){
+    res.status(200).json({
+      error:error
+    })
+  }  
 
-  res.status(200).json ({msg:"testing ok"});
 };
 
 export { 
